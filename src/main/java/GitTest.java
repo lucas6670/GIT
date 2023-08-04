94353d0d1c1635fdb4b9a32f89ab12ebb3de2f07
lucas <1635428106@qq.com>
2023-08-04 09:52:58 +0800

    hot first commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,6 +3,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello Git");
-
+        System.out.println("branch");
     }
 }
